ea02123d169173870113711db556f228582589e4
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-06-26 00:36:18 +0300

    feat: hash password with bcrypt before saving user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,15 +1,22 @@
 import { IUser, User } from "../models/user.model";
+import bcrypt from 'bcrypt';
+
+export const BCRYPT_SALT_ROUNDS = parseInt(process.env.BCRYPT_SALT_ROUNDS || '10');
 
 export const createUser = async (payload: IUser) => {
     console.log("Service Payload", payload);
+
+    const hashedPassword = await bcrypt.hash(payload.password, BCRYPT_SALT_ROUNDS);
+
     const user = await User.create({
-        username: payload.username,
-        password: payload.password,
-        email: payload.email,
+        username:  payload.username,
+        password:  hashedPassword,
+        email:     payload.email,
         firstName: payload.firstName,
-        lastName: payload.lastName,
-        roleId: 2
+        lastName:  payload.lastName,
+        roleId:    2,
     });
+
     console.log("New user: ", user);
     return user;
-}
+};
