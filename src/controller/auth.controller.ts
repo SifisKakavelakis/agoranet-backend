d943bdf8000aea6e1c042612c66021459b7417ed
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-07-01 17:58:06 +0300

    feat: add auth controller with register and login handlers

---
+++ src/controller/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import * as authService from '../services/auth.service';
+import { CreateUserDTO } from '../dto/user.dto';
+import { toUserResponseDTO } from '../mappers/user.mapper';
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { credential, password } = req.body;
+        const result = await authService.login(credential, password);
+        if (!result) return res.status(401).json({ message: 'Invalid email or password' });
+        res.status(200).json({ message: 'Login successful', data: toUserResponseDTO(result) });
+    } catch (err) {
+        next(err);
+    }   
+}
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const data: CreateUserDTO = req.body;
+        const result = await authService.register(data);
+        if (!result) return res.status(400).json({ message: 'User already exists' });
+        res.status(200).json({ message: 'User registered successfully', data: toUserResponseDTO(result) });
+    } catch (err) {
+        next(err);
+    }
+};
