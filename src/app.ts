5baf843dd2f327bcf2c74f55865226d1d51b4f39
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-07-01 18:01:32 +0300

    feat: add auth routes to express app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes';
 import './models/index'; 
 
 dotenv.config();
@@ -9,5 +10,6 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
 
 export default app;
