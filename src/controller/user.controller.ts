77940b555f3e8a95ff183210b678b2173661ff1c
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-07-05 16:40:45 +0300

    fix: add user mapper in update controller to hide password and UserRole

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import * as userService from '../services/user.service';
 import { UpdateUserDTO } from "../dto/user.dto";
+import { toUserResponseDTO } from "../mappers/user.mapper";
 
 export const update = async(req:Request, res:Response, next:NextFunction) => {
     try {
@@ -8,9 +9,9 @@ export const update = async(req:Request, res:Response, next:NextFunction) => {
         const data: UpdateUserDTO = req.body;
         const result = await userService.updateUser(username, data);
         if (!result) {
-            return res.status(401).json({message: 'User not found' });
+            return res.status(404).json({message: 'User not found' });
         }
-        res.status(200).json(result);
+        res.status(200).json(toUserResponseDTO(result));
     } catch(err) {
         next(err)
     }
