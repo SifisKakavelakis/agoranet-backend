10ecb9d3ddba2cc5d3968ba8f4b226bf59a59409
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-07-07 14:31:07 +0300

    feat: update models index with product, category and image associations

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,9 @@
 import { User } from './user.model';
 import { Role } from './role.model';
 import { UserRole } from './user-role.model';
+import Product from './product.model';
+import Category from './category.model';
+import ProductImage from './product-image.model';
 
 User.belongsToMany(Role, {
     through:    UserRole,
@@ -16,4 +19,13 @@ Role.belongsToMany(User, {
     as:         'users',
 });
 
-export { User, Role, UserRole };
+User.hasMany(Product, { foreignKey: 'seller_id', as: 'products' });
+Product.belongsTo(User, { foreignKey: 'seller_id', as: 'seller' });
+
+Category.hasMany(Product, { foreignKey: 'category_id', as: 'products' });
+Product.belongsTo(Category, { foreignKey: 'category_id', as: 'category' });
+
+Product.hasMany(ProductImage, { foreignKey: 'product_id', as: 'images' });
+ProductImage.belongsTo(Product, { foreignKey: 'product_id', as: 'product' });
+
+export { User, Role, UserRole, Product, ProductImage, Category };
